5b52b69d5ccdbef63a23f7186137c31c345d0fc3
Tim <tim.stone@yahoo.co.uk>
2022-11-30 15:36:02 +0000

    Updated proposal overview ids

--- features/page_objects/confirm_page.rb
+++ features/page_objects/confirm_page.rb
@@ -2,7 +2,7 @@ class ConfirmPage < BasePage
 
   section(:user_bar, AdminUserBarSection, AdminUserBarSection::SELECTOR)
 
-  element(:project_number, ".govuk-panel__body .heading-medium")
+  element(:project_number, ".govuk-panel__body strong")
   element(:return_to_proposals, "a[href='pc/projects']")
 
 end
