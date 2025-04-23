06ab5b88e7acd784cf6c30a2488c0ebd6013ace2
Tim Stone <tim.stone@yahoo.co.uk>
2025-04-23 12:12:32 +0100

    Bundle update (#196)

--- features/page_objects/back_office/back_office_home_page.rb
+++ features/page_objects/back_office/back_office_home_page.rb
@@ -2,7 +2,7 @@ require_relative "base_page"
 
 class BackOfficeHomePage < BasePage
 
-  set_url((Quke::Quke.config.custom["urls"]["back_office"]).to_s)
+  set_url(Quke::Quke.config.custom["urls"]["back_office"].to_s)
   element(:start_button, ".button.button-start")
 
 end
--- features/page_objects/front_office_home_page.rb
+++ features/page_objects/front_office_home_page.rb
@@ -2,7 +2,7 @@ require_relative "base_page"
 
 class FrontOfficeHomePage < BasePage
 
-  set_url((Quke::Quke.config.custom["urls"]["front_office"]).to_s)
+  set_url(Quke::Quke.config.custom["urls"]["front_office"].to_s)
   element(:start_button, ".button.button-start")
 
 end
