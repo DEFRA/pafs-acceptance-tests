cfb351483d06121905307f2edb044d5c5df884b1
Tim Stone <tim.stone@yahoo.co.uk>
2025-08-05 09:52:25 +0100

    PFC version update (#198)

--- features/page_objects/funding_calculator_page.rb
+++ features/page_objects/funding_calculator_page.rb
@@ -8,14 +8,6 @@ class FundingCalculatorPage < BasePage
   element(:choose_file, "#funding-calculator-step-funding-calculator-field")
 
   def submit(args = {})
-    case args[:option]
-    when :Yes
-      signed_off_after.click
-
-    when :No
-      signed_off_before.click
-    end
-
     if args.key?(:file)
       filename = args[:file]
       attach_file(choose_file[:name], File.absolute_path("./features/support/#{filename}"))
--- features/step_definitions/funding_calculator_upload_steps.rb
+++ features/step_definitions/funding_calculator_upload_steps.rb
@@ -1,14 +1,6 @@
 Given(/^I upload a project funding calculator file "([^"]*)"$/) do |filename|
-  @filename = filename
-  @app.funding_calculator_page.submit(
-    file: @filename
-  )
-end
-
-Given(/^I upload a project funding calculator with file option "([^"]*)", "([^"]*)"$/) do |radiotype, filename|
   @app.proposal_overview_page.add_funding_calculator.click
   @app.funding_calculator_page.submit(
-    option: radiotype.to_sym,
     file: filename
   )
   expect(@app.funding_calculator_summary_page.uploaded_file).to have_text(filename)
