1d97d7d27d2d5dbe123ca8539abb4999ee6bffef
Tim Stone <tim.stone@yahoo.co.uk>
2025-08-07 09:58:41 +0100

    Sign in selector change (#202)

--- features/page_objects/back_office/back_office_login_page.rb
+++ features/page_objects/back_office/back_office_login_page.rb
@@ -4,12 +4,13 @@ class BackOfficeLoginPage < BasePage
 
   element(:email, "#user-email-field")
   element(:password, "#user-password-field")
+  element(:sign_in, "#new_user [type='submit']")
 
   def submit(args = {})
     email.set(args[:email]) if args.key?(:email)
     password.set(args[:password]) if args.key?(:password)
 
-    submit_button.click
+    sign_in.click
   end
 
 end
